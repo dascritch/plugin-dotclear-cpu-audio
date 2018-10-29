a17349addb96ac2a1f11fe6b1037d0f521a5f871
dascritch <xaviermd@gmail.com>
2018-10-29 12:59:21 +0200

    Going to cpu-audio

--- cpu-audio-public/_public.php
+++ cpu-audio-public/_public.php
@@ -2,11 +2,35 @@
 
 if (!defined('DC_RC_PATH')) { return; }
 
-$core->tpl->addValue('OggFile',                     ['DSN_audio_tpl','OggFile']);
-$core->tpl->addValue('ChapterFile',                 ['DSN_audio_tpl','ChapterFile']);
-$core->tpl->addBlock('HasChapterFile',				['DSN_audio_tpl','HasChapterFile']);
+$core->addBehavior('publicHeadContent', ['CPU_Audio_behaviors', 'publicHeadContent']);
+$core->addBehavior('publicBeforeDocument', ['CPU_Audio_behaviors', 'addMP3template']);
 
-class DSN_audio_tpl
+$core->tpl->addValue('OggFile',                     ['CPU_Audio_tpl','OggFile']);
+$core->tpl->addValue('ChapterFile',                 ['CPU_Audio_tpl','ChapterFile']);
+$core->tpl->addBlock('HasChapterFile',				['CPU_Audio_tpl','HasChapterFile']);
+
+/**
+$this->tpl_path  [] = $core->getPF('default-templates')
+**/
+
+
+class CPU_Audio_behaviors
+{
+    public static function publicHeadContent($core) {
+        return $core->util->jsLoad($core->getPF('js/cpu-audio.js'));
+    }
+
+    public static function addMP3template($core) {
+        $core->tpl->setPath(
+            dirname(__FILE__) . '/default-templates',
+            $core->tpl->getPath());
+
+
+    }
+}
+
+
+class CPU_Audio_tpl
 {
 	public static function OggFile($attr) {
 		return '<?php
