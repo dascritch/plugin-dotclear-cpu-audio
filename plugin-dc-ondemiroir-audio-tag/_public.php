029382ffc2628ae551a64c7992712eb5394597d1
Xavier Mouton-Dubosc <xavier@dascritch.com>
2019-04-05 20:14:14 +0200

    Update for Waveform file support in cpu-audio.js v5.5

--- plugin-dc-ondemiroir-audio-tag/_public.php
+++ plugin-dc-ondemiroir-audio-tag/_public.php
@@ -5,18 +5,20 @@ if (!defined('DC_RC_PATH')) { return; }
 $core->tpl->addValue('OggFile',                     ['DSN_audio_tpl','OggFile']);
 $core->tpl->addValue('ChapterFile',                 ['DSN_audio_tpl','ChapterFile']);
 $core->tpl->addBlock('HasChapterFile',				['DSN_audio_tpl','HasChapterFile']);
+$core->tpl->addValue('WaveformFile',                ['CPU_Audio_tpl','WaveformFile']);
+$core->tpl->addBlock('HasWaveformFile',             ['CPU_Audio_tpl','HasWaveformFile']);
 
 class DSN_audio_tpl
 {
-	public static function OggFile($attr) {
-		return '<?php
-					$oggpossible = preg_replace(
+    public static function OggFile($attr) {
+        return '<?php
+                    $oggpossible = preg_replace(
                         ["/\.mp3/", "/\/podcast\//"],
                         [".ogg", "/"],
                         $attach_f->file_url);
-					echo $oggpossible;
-				?>';
-	}
+                    echo $oggpossible;
+                ?>';
+    }
 
     public static function ChapterFile($attr) {
         return '<?php
@@ -34,11 +36,27 @@ class DSN_audio_tpl
                         ["/\.mp3/", "/\/podcast\//"],
                         [".vtt", "/tracks/"],
                         $attach_f->file_url);
-                    $chapterpossiblefile = preg_replace(
+                    if (file_exists($chapterpossible)) { 
+                ?>'.$content."<?php } ?>\n";
+    }
+
+    public static function WaveformFile($attr) {
+        return '<?php
+                    $waveformpossible = preg_replace(
                         ["/\.mp3/", "/\/podcast\//"],
-                        [".vtt", "/tracks/"],
+                        [".png", "/waveform/"],
+                        $attach_f->file_url);
+                    echo $waveformpossible;
+                ?>';
+    }
+
+    public static function HasWaveformFile($attr, $content) {
+        return '<?php
+                    $waveformpossible = preg_replace(
+                        ["/\.mp3/", "/\/podcast\//"],
+                        [".png", "/waveform/"],
                         $attach_f->file);
-                    if (file_exists( $chapterpossiblefile)) { 
+                    if (file_exists($waveformpossible)) { 
                 ?>'.$content."<?php } ?>\n";
     }
 
